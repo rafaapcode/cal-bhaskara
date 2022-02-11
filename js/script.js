f157ef1ded4edf3b5a72c660bc351d37ed2e6b24
rafaapcode <rafaap2003@gmail.com>
2022-02-11 05:14:15 -0300

    jquery

--- js/script.js
+++ js/script.js
@@ -1,41 +1,41 @@
-const a = document.querySelector("#a");
-const b = document.querySelector("#b");
-const c = document.querySelector("#c");
-const btn = document.querySelector("#btn");
-const valornatela = document.querySelector(".deltaValor");
-const x1 = document.querySelector(".x1");
-const x2 = document.querySelector(".x2");
+const a = $("#a");
+const b = $("#b");
+const c = $("#c");
+const btn = $("#btn");
+const valornatela = $(".deltaValor");
+const x1 = $(".x1");
+const x2 = $(".x2");
 
 
 const bhaskara = function () {
-    const bQuadrado = b.value * b.value;
-    const mAeC = a.value * c.value;
+    const bQuadrado = b.val() * b.val();
+    const mAeC = a.val() * c.val();
     const meno4 = -4 * mAeC;
 
     const result = bQuadrado + meno4;
 
-    valornatela.innerHTML = `${result}`;
+    valornatela.text(`${result}`);
 
     if (result >= 0) {
-        const div = 2 * a.value;
-        const bneg = -b.value;
+        const div = 2 * a.val();
+        const bneg = -b.val();
         const quadradaDelta = Math.sqrt(result);
 
         const x1calc = (bneg + quadradaDelta) / div;
         const x2calc = (bneg - quadradaDelta) / div;
 
-        x1.innerHTML = `O x1 é igual a: ${Math.round(x1calc)}`;
-        x2.innerHTML = `O x2 é igual a: ${Math.round(x2calc)}`;
-        x2.style.display = "block";
+        x1.text(`O x1 é igual a: ${Math.round(x1calc)}`);
+        x2.text(`O x2 é igual a: ${Math.round(x2calc)}`);
+        x2.css("display", "block");
     }else{
-        x1.innerHTML = `Não existe raiz de Número negativo.`;
-        x2.style.display = "none";
+        x1.text(`Não existe raiz de Número negativo.`);
+        x2.css("display", "none")
     }
 }
 
 
 
-btn.addEventListener("click", bhaskara);
+btn.click(bhaskara);
 
 
 
